e11d626d28b0620e288e80a1e5828b7f4adefcaa
alejandra.pinzon <ma.pinzons1@uniandes.edu.co>
2025-09-25 03:18:29 -0500

    Page Home

--- uxweb/src/app/pages/home/home.component.ts
+++ uxweb/src/app/pages/home/home.component.ts
@@ -1,15 +1,21 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+// Angular Material
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-home',
-  standalone: true,    
+  standalone: true,
   imports: [
-    CommonModule,      
-    MatCardModule,     
-    MatButtonModule    
+    CommonModule,
+    MatCardModule,
+    MatButtonModule,
+    MatIconModule,
+    MatToolbarModule
   ],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
