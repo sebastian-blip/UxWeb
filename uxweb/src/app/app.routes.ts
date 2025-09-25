4d6c777700bb8ddcc73366c4f8c495539e9401a1
alejandra.pinzon <ma.pinzons1@uniandes.edu.co>
2025-09-24 23:51:13 -0500

    Componente inicial page - home

--- uxweb/src/app/app.routes.ts
+++ uxweb/src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
 import { MenusPageComponent } from './pages/menus-page/menus-page.component';
 
 export const routes: Routes = [
+  { path: '', component: HomeComponent },
   { path: 'menus', component: MenusPageComponent },
   // ... otras rutas
 ];
